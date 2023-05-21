c158a168dbf97255a98fd87a5938738f3fa9a45b
someonempty <someonempty@gmail.com>
2023-05-21 18:25:14 +0300

    Декомпозицая конфига и добавление в него опций

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import webpack, { WebpackPluginInstance } from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import { BuildOptions } from './types/config';
+
+export function buildPlugins({ paths }: BuildOptions): webpack.WebpackPluginInstance[] {
+	return [
+		new HtmlWebpackPlugin({
+			template: paths.html,
+		}),
+		new webpack.ProgressPlugin(),
+	];
+}
