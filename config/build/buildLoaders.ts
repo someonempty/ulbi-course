c1b0c0064e2543694c53c0709ac9f10e2445e111
someonempty <someonempty@gmail.com>
2023-05-29 22:37:40 +0300

    Настраивает css modules часть 2

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -3,15 +3,18 @@ import webpack from 'webpack';
 import { BuildOptions } from './types/config';
 import loader from 'mini-css-extract-plugin/types/loader';
 
-export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
-			options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
 			{
 				loader: 'css-loader',
 				options: {
-					modules: true,
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev ? '[path][name]__[local]--[hash:base64:5]' : '[hash:base64:8]',
+					},
 				},
 			},
 			'sass-loader',
